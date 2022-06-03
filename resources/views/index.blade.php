7029c13589308c9711e18208dd153172bec929eb
Anittamolchacko <anittamolchacko@mca.ajce.in>
2022-06-03 09:25:41 +0530

    second commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1> hai ALL</h1>
+   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae numquam consequuntur officia nobis accusamus sed, delectus soluta obcaecati distinctio iure itaque earum ratione sapiente ut quas dolores quae qui corporis.</p>
+</body>
+</html>
